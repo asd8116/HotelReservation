36bff56e5b8ad67c7002d3c7cc1e8e15f3803659
wanaka <asd8116@gmail.com>
2019-12-09 23:49:16 +0800

    Modify Home.vue & BackgroundImage.vue

---
+++ src/components/BackgroundImage.vue
@@ -0,0 +1,103 @@
+<template>
+  <div
+    class="background-image"
+    v-if="iniRoom"
+    :style="{backgroundImage: `url(${iniRoom.imageUrl})`}"
+  >
+    <div class="show"></div>
+
+    <div class="room-index-name">
+      <div class="number">{{ getStringNumber }}</div>
+      <div class="name">{{ iniRoom.name }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    iniRoom: {
+      type: Object,
+      default () {
+        return {
+          imageUrl: 'https://via.placeholder.com/1200'
+        }
+      }
+    },
+    iniIndex: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    getStringNumber () {
+      const stringNumber = this.iniIndex.toString()
+
+      return '0' + stringNumber
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.background-image {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center bottom;
+
+  .show {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(192, 192, 192, 0.2);
+    background-image: linear-gradient(180deg, rgba(0, 0, 0, 0) 35%, rgba(0, 0, 0, 0.35) 100%);
+  }
+  .room-index-name {
+    position: absolute;
+    left: 70px;
+    bottom: 75px;
+
+    .number {
+      font-size: 66px;
+      font-weight: normal;
+      color: white;
+
+      &::after {
+        content: '';
+        width: 85px;
+        height: 20px;
+        position: absolute;
+        top: 70%;
+        left: 0;
+        background-color: rgba(white, 0.2);
+        background-image: linear-gradient(
+          45deg,
+          white 5%,
+          transparent 5%,
+          transparent 45%,
+          white 45%,
+          white 55%,
+          transparent 55%,
+          transparent 95%,
+          white 95%,
+          white
+        );
+        background-size: 10px 10px;
+      }
+    }
+    .name {
+      margin-top: 12px;
+      font-size: 24px;
+      letter-spacing: 2.51px;
+      color: white;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,8 @@
 <template>
   <main class="landing-page">
-    <transition name="fade" v-for="room in rooms" :key="room.id">
-      <BackgroundImage :initial-room="room" v-if="hoverRoomId === room.id" />
+    <transition name="fade" v-for="(room, index) in rooms" :key="index">
+      <BackgroundImage :ini-room="room" :ini-index="index + 1" v-if="hoverRoomId === room.id" />
     </transition>
-
-    <div class="show"></div>
 
     <router-link to="/rooms" class="img">
       <img :src="`${basePath}images/logo_white.svg`" alt />
@@ -19,10 +17,42 @@
           :key="room.id"
           @mouseover="mouseOver(room.id)"
           @mouseout="isPaused = false"
-        >{{ room.name }}</li>
+        >
+          <router-link :to="{ name: 'room', params: { roomId: room.id }}">{{ room.name }}</router-link>
+        </li>
       </ul>
 
-      <div class="information"></div>
+      <div class="information">
+        <div class="social">
+          <a href class="social-link facebook">
+            <img :src="`${basePath}images/facebook-square-brands.svg`" alt />
+          </a>
+          <a href class="social-link instagram">
+            <img :src="`${basePath}images/instagram-brands.svg`" alt />
+          </a>
+        </div>
+
+        <ul class="contact">
+          <li>
+            <span class="icon">
+              <img :src="`${basePath}images/phone-alt-solid.svg`" alt />
+            </span>
+            <div class="contant">02-17264937</div>
+          </li>
+          <li>
+            <span class="icon">
+              <img :src="`${basePath}images/envelope-solid.svg`" alt />
+            </span>
+            <div class="contant">whitespace@whitespace.com.tw</div>
+          </li>
+          <li>
+            <span class="icon">
+              <img :src="`${basePath}images/home-solid.svg`" alt />
+            </span>
+            <div class="contant">台北市羅斯福路十段30號</div>
+          </li>
+        </ul>
+      </div>
     </div>
   </main>
 </template>
@@ -53,9 +83,11 @@ export default {
     async fetchRooms () {
       const vm = this
       try {
+        let loader = this.$loading.show()
         const { data } = await roomsAPI.getRooms()
 
         vm.rooms = data.items
+        loader.hide()
         Toast.fire({
           icon: 'success',
           title: 'Get All Rooms'
@@ -78,41 +110,64 @@ export default {
 
 <style lang="scss" scoped>
 main.landing-page {
-  padding-top: 55px;
-  padding-left: 45px;
+  width: 100%;
+  height: 100%;
+  padding: 55px 0 35px 45px;
   display: flex;
   justify-content: space-between;
-  .show {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(192, 192, 192, 0.2);
-    background-image: linear-gradient(180deg, rgba(0, 0, 0, 0) 35%, rgba(0, 0, 0, 0.35) 100%);
-  }
+
   .img {
     height: 170px;
+  }
+  .list-infor {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
   }
   .rooms-List {
     background-color: white;
     list-style: none;
-    padding-top: 18px;
-    padding-left: 34px;
-    padding-bottom: 44px;
-    padding-right: 34px;
+    padding: 18px 34px 50px;
     margin: 0;
     min-width: 305px;
+
+    &::after {
+      content: '';
+      width: 280px;
+      height: 20px;
+      position: absolute;
+      top: 100%;
+      right: 0;
+      background-color: rgba(white, 0.2);
+      background-image: linear-gradient(
+        45deg,
+        white 5%,
+        transparent 5%,
+        transparent 45%,
+        white 45%,
+        white 55%,
+        transparent 55%,
+        transparent 95%,
+        white 95%,
+        white
+      );
+      background-size: 10px 10px;
+    }
+    li a {
+      color:#000;
+    }
     li.room {
-      padding: 18px 0;
+      padding: 16px 0;
       font-size: 14px;
       letter-spacing: 1.5px;
       cursor: pointer;
+
       &::before {
         content: '';
         display: inline-block;
         width: 23px;
-        height: 2px;
+        height: 3px;
         background-color: #696969;
         opacity: 0;
         visibility: hidden;
@@ -126,6 +181,51 @@ main.landing-page {
         &::before {
           opacity: 1;
           visibility: visible;
+        }
+      }
+    }
+  }
+  .information {
+    width: 100%;
+    padding-left: 25px;
+
+    .social {
+      display: flex;
+
+      .social-link {
+        width: 25px;
+        height: 25px;
+        margin-right: 15px;
+        img {
+          width: 100%;
+          object-fit: cover;
+        }
+      }
+    }
+    ul.contact {
+      list-style: none;
+      padding: 0;
+      margin-top: 50px;
+
+      li {
+        display: flex;
+        margin-bottom: 14px;
+        &:last-child {
+          margin-bottom: 0;
+        }
+
+        .icon {
+          width: 20px;
+          height: 20px;
+          img {
+            object-fit: cover;
+          }
+        }
+        .contant {
+          font-size: 14px;
+          color: white;
+          margin-top: 3px;
+          margin-left: 12px;
         }
       }
     }
