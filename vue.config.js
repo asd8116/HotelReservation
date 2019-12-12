83bdcd43e9459df4506da12f4206d015b24cc44e
wanaka <asd8116@gmail.com>
2019-12-12 18:13:39 +0800

    Add deploy.sh and vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/HotelReservation/' : '/',
   css: {
     loaderOptions: {
       scss: {
